517a00c0bd9abc8b7ca262bc880005fee04eaf69
ranat <ranat@DESKTOP-FD6U1RI>
2019-03-29 14:54:33 +0530

    second commit
    uploading string concatenation

---
+++ Newjavasession/src/oops12/Stringconcatenation12.java
@@ -0,0 +1,27 @@
+package oops12;
+
+public class Stringconcatenation12 {
+
+	public static void main(String[] args) {
+
+		
+		int i = 20;
+		int j = 30;
+		
+		double d = 54.2;
+		double e = 45.2;
+		
+		String x = " Selenium ";
+		String s = "Automation";
+		
+		System.out.println(i + j + d + e + x + s );
+		System.out.println(i + j + d + e );
+		System.out.println(i + j +(x + s));
+		System.out.println(x + s +(i + j));
+		
+		
+		System.out.println("the value of i is "+i);
+		System.out.println("the value of s is "+ s);
+	}
+
+}
