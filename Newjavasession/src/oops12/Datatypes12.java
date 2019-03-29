b4bdb1d53104f56f259c058f809c26330397c388
ranat <ranat@DESKTOP-FD6U1RI>
2019-03-29 14:34:41 +0530

    new java session
    uploading data types

---
+++ Newjavasession/src/oops12/Datatypes12.java
@@ -0,0 +1,42 @@
+package oops12;
+
+public class Datatypes12 {
+	//main method -> starting and execution point of the program
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//primitive data type: int , double , char , boolean 
+		
+		//1. int
+		int i = 10;
+		int j = 20;
+		int k = 54;
+		System.out.println(i - j + k);
+		
+		//int m = 87.43;//we can't write this 
+		
+		//2. double
+		double d = 345.33;
+		double e = 100;
+		System.out.println(d + e);
+		//3. char
+		char c = 'a';
+		System.out.println(c);
+		char c1 = 'A';
+		System.out.println(c1);
+		char c2 = '$';
+		//4. boolean
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		
+		//string is a class , not a data type
+		String s = "selenium";
+		System.out.println(s);
+		String s1 = "this is my first code";
+		String s2 = "1000";
+		System.out.println(s1  +  s2);		
+	}
+
+}
