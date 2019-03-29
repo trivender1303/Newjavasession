227bf5925bb96394b2005e4968767e8b476de8c9
ranat <ranat@DESKTOP-FD6U1RI>
2019-03-29 16:45:10 +0530

    uploaded loops concepts

---
+++ Newjavasession/src/oops12/Loops12.java
@@ -0,0 +1,30 @@
+package oops12;
+
+public class Loops12 {
+
+	public static void main(String[] args) {
+
+		// while loop
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i);
+			i = i + 1;
+		}
+
+		System.out.println("**********");
+
+		// for loop
+
+		for (int j = 1; j <= 10; j++) {
+			System.out.println(j);
+		}
+		System.out.println("************");
+		//writing 10 to 1
+		for(int g=10;g>=0;g--){
+			
+			System.out.println(g);
+		}
+
+	}
+
+}
